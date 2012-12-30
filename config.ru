63d521960dcacddf3ea378af410a6902bfe282fc
Andrew Stewart <andrew@averagestudios.com>
2012-12-30 11:22:37 -0800

    Inlined Bootstrap and switched to the sass plugin for rack

--- config.ru
+++ config.ru
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'bundler'
 Bundler.require(:default)
 
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
+
 require './site'
 require './lib/stats'
 
--- site.rb
+++ site.rb
@@ -3,11 +3,6 @@ get "/" do
   haml :'index.html'
 end
 
-get '/application.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss :application
-end
-
 get "/stats" do
   @stats = Stats.get_totals
   haml :'stats.html', :layout => false
