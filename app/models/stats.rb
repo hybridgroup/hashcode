4b44e02aed6a075354a01c3cb30392e68e7761fc
Andrew Stewart <andrew@stwrt.ca>
2013-12-22 19:39:21 -0800

    Add method to return sorted stats hash

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -5,6 +5,10 @@ class Stats
   property :stats, Json
   property :created_at, DateTime
 
+  def sorted_stats
+    Hash[stats.sort_by { |_, count| count }.reverse]
+  end
+
   class << self
     def last
       first :order => [ :created_at.desc ]
