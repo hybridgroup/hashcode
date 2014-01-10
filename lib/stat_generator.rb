90224485f06a10a107bcf0e48fef4bccec28424d
Luis Felipe Perez <slashcat@gmail.com>
2014-01-09 18:46:07 -0600

    more actionscript options

--- lib/stat_generator.rb
+++ lib/stat_generator.rb
@@ -2,7 +2,7 @@ class StatGenerator
   # A map of language names to possible shorthand versions.
   LANGS = {
     "Abap" => %w(abap),
-    "ActionScript" => %w(actionscript action_script as3),
+    "ActionScript" => %w(actionscript action_script as3 as2 as1 flash),
     "Ada" => %w(ada),
     "Apl" => %w(apl),
     "Assembly" => %w(assembly),
